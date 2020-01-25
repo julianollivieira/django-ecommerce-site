fcd99f36fe5715790c0f3012b50c4f8ac65a6068
Julian <julian.ollivieira@gmail.com>
2020-01-25 12:56:00 +0100

    Add user model
    Removed '/webshop' route prefix, cleanedup css file and added a user model,

--- django_ecommerce_site/django_ecommerce_site/urls.py
+++ django_ecommerce_site/django_ecommerce_site/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webshop/', include('webshop.urls')), #path('')
+    path('', include('webshop.urls')), #path('')
 ]
--- django_ecommerce_site/webshop/models.py
+++ django_ecommerce_site/webshop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=32)
+
+    gender = models.BooleanField()
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+
+    postal_code = models.CharField(max_length=16)
+    house_number_additions = models.CharField(max_length=16)
+    street_name = models.CharField(max_length=64)
+    city_name = models.CharField(max_length=32)
+    country = models.CharField(max_length=32)
+
+    phone_number = models.CharField(max_length=16)
+    date_of_birth = models.DateField()
